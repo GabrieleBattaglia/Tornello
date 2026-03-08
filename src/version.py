22920bfc953526621b759d32de0f53d9b46db14f
GabrieleBattaglia <iz4apu@libero.it>
2026-03-08 14:12:45 +0100

    Bump version to 8.7.0 and add Stella GeminiPro3.1 as author

--- src/version.py
+++ src/version.py
@@ -1,6 +1,6 @@
-__version__ = "8.6.11"
+__version__ = "8.7.0"
 __date__ = "2026.03.08"
-__authors__ = "Gabriele Battaglia & Stella"
+__authors__ = "Gabriele Battaglia & Stella GeminiPro3.1"
 __bbp_info__ = "using BBP Pairings, a Swiss-system chess tournament engine created by Bierema Boyz Programming."
 
 VERSIONE = f"{__version__}, {__date__} by {__authors__}\n\t{__bbp_info__}"
